83e77f5f1e4d989fd67a35e37558e6ad23f17e45
Rodrigo Silva <jose.leandro@dotgroup.com.br>
2024-01-23 18:24:55 -0300

    principais metodos do tipo texto

---
+++ Modulo_1/tipos_variaveis/principais-metodos-do-tipo-texto.py
@@ -0,0 +1,53 @@
+# nome = "Gabriel"
+# sobrenome = "Casemiro"
+# nome_completo = "Gabriel Casemiro"
+
+# print(nome.upper())
+# print(nome.lower())
+# print(nome[0])
+# print(nome_completo.count("a"))
+# print(nome_completo.find("a"))
+# print(nome.encode())
+# print(nome.encode().decode())
+# print(nome_completo.replace("b", "a"))
+
+# telefone = "(19)97325-0502"
+# print(telefone.replace("(", "").replace(")", "").replace("-", ""))
+
+# print("-".join(nome))
+# print(nome_completo.split(" "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# nome = "xGabriel Casemirox"
+# print(nome.strip("x"))
+# print(nome.rstrip("x"))
+# print(nome.lstrip("x"))
+
+
+
+
+
+
+
+
+
+
+
+
+nome_completo = "Gabriel Casemiro"
+print(nome_completo.startswith("Ga"))
+print(nome_completo.endswith("ro"))
+print("el" in nome_completo)
+print("el" not in nome_completo)
