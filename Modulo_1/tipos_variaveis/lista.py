5a3ede4e0345c35c90e4fb7fdd08881faa408926
Rodrigo Silva <jose.leandro@dotgroup.com.br>
2024-01-24 14:05:50 -0300

    listas

---
+++ Modulo_1/tipos_variaveis/lista.py
@@ -0,0 +1,11 @@
+# Declaração
+
+minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+# Exibindo a lista
+print("Minha lista de exemplo", minha_lista)
+
+# Exibindo a lista
+print(minha_lista[0])
+print(minha_lista[5])
+print(minha_lista[1:7])
