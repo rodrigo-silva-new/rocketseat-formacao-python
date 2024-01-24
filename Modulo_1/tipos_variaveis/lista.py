3557aaf731687f2cd55f9a6207adebbef7cfda0c
Rodrigo Silva <jose.leandro@dotgroup.com.br>
2024-01-24 14:35:58 -0300

    finaliza lista

--- Modulo_1/tipos_variaveis/lista.py
+++ Modulo_1/tipos_variaveis/lista.py
@@ -6,6 +6,8 @@ minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
 print("Minha lista de exemplo", minha_lista)
 
 # Exibindo a lista
-print(minha_lista[0])
-print(minha_lista[5])
-print(minha_lista[1:7])
+print("minha_lista[0]:", minha_lista[0])
+print("minha_lista[5]:", minha_lista[5])
+print("minha_lista[1:7]:", minha_lista[1:7])
+print("minha_lista[:6]:", minha_lista[:6])
+print("minha_lista[2:]:", minha_lista[2:])
