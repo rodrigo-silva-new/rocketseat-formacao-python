9786ef84c6f8f7485847c3910a000b03f2ad53c4
Rodrigo Silva <jose.leandro@dotgroup.com.br>
2024-01-29 14:04:10 -0300

    loop while

---
+++ Modulo_1/projeto/gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+  print("\nMenu do gerenciador de lista de tarefas:")
+  print("1. Adicionar uma Tarefa")
+  print("2. Ver Tarefas")
+  print("3. Atualizar Tarefa")
+  print("4. Completar Tarefa")
+  print("5. Deletar tarefas completadas")
+  print("6. Sair")
+
+  escolha = input("Digite a sua escolha: ")
+
+  if escolha == "6":
+    break
+
+print("Programa finalizado")
---
+++ Modulo_1/tipos_variaveis/17-loop-while.py
@@ -0,0 +1,7 @@
+print("Exemplo de while")
+contador = 0
+while contador < 5:
+  print("Contage:", contador)
+  contador += 1
+
+print("Programa finalizado")
