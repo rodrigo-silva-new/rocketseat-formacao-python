c791b4f19ca12f4c9b5a3ac3a28bf6d78c7bb106
Rodrigo Silva <rodrigonew290894@gmail.com>
2024-03-11 23:08:31 -0300

    continuar em "numeros - parte 2"

---
+++ Modulo_1/tipos_variaveis/numeros.py
@@ -0,0 +1,11 @@
+# Inteiro
+numero_inteiro = 3
+print("Inteiro = ", numero_inteiro)
+
+# Real com ponto flutuante
+numero_real = 3.14
+print("Real com ponto flutuante = ", numero_real)
+
+# type()
+print("Tipo da variavel inteiro = ", type(numero_inteiro))
+print("Tipo da variavel inteiro = ", type(numero_real))
