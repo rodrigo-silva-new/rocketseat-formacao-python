ba52343ffeff2efb736098e12494b1a65744a6c2
Rodrigo Silva <rodrigonew290894@gmail.com>
2024-03-12 19:41:23 -0300

    feito ate numeros - parte 2

--- Modulo_1/tipos_variaveis/numeros.py
+++ Modulo_1/tipos_variaveis/numeros.py
@@ -9,3 +9,36 @@ print("Real com ponto flutuante = ", numero_real)
 # type()
 print("Tipo da variavel inteiro = ", type(numero_inteiro))
 print("Tipo da variavel inteiro = ", type(numero_real))
+
+# soma +
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+# subtracao -
+subtracao = 1 - 1
+print("1 - 1 = ", subtracao)
+
+# multiplicacao *
+multiplicacao = 2 * 2
+print("2 x 2 = ", multiplicacao)
+
+# divisão /
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+print("Tipo da variável do resultado da divisão = ", type(divisao))
+
+# divisão inteiro //
+divisao = 5 // 2
+print("5 // 2 = ", divisao)
+print("Tipo da variável do resultado da divisão = ", type(divisao))
+
+# int()
+print("Valor em inteiro = ", int(divisao))
+
+# float()
+print("Valor em float = ", float(divisao))
+
+
+# Módulo %
+modulo = 5 % 2
+print("Modulo = ", modulo)
