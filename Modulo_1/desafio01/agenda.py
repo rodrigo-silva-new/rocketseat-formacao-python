ccb76709b766ea19f58488dce27ece7368aa3187
Rodrigo Silva <rodrigonew290894@gmail.com>
2024-04-10 19:17:31 -0300

    finaliza o desafio 01 "agenda.py"

--- Modulo_1/desafio01/agenda.py
+++ Modulo_1/desafio01/agenda.py
@@ -35,8 +35,46 @@ def editar_contato(contatos, indice_contato):
       print("Edição de contato encerrada!")
       break
 
+def favoritos(contatos):
+  while True:
+    print("\n1. Favoritar contato")
+    print("2. Remover contato dos favoritos")
+    print("3. Ver contatos")
+    print("4. Encerrar")
+
+    escolha = int(input("Digite a opção que deseja: "))
+
+    if escolha == 1:
+      ver_contatos(contatos)
+      indice_contato = int(input("Digite o número do contato que deseja favoritar: "))
+      contatos[indice_contato - 1]["favorito"] = True
+      print("Contato favoritado com sucesso")
+    elif escolha == 2:
+      ver_contatos(contatos)
+      indice_contato = int(input("Digite o número do contato que deseja remover de favoritos: "))
+      contatos[indice_contato - 1]["favorito"] = False
+      print("Contato removido dos favoritos com sucesso")
+    elif escolha == 3:
+      ver_contatos(contatos)
+    elif escolha == 4:
+      break
+
+def ver_favoritos(contatos):
+  print("\nLista de favoritos:")
+  for indice, contato in enumerate(contatos, start=1):
+    if contato["favorito"] == True:
+      print(contato)
+
+def apagar_contato(contatos, indice_contato):
+  contato_excluir = indice_contato
+
+  for indice, contato in enumerate(contatos, start=1):
+    if indice == contato_excluir:
+      contatos.remove(contato)
+  print("Contato apagado com sucesso!")
+      
+
 contatos = []
-favoritos = []
 
 while True:
   print("\n1. Adicionar um contato")
@@ -61,5 +99,14 @@ while True:
     ver_contatos(contatos)
     indice_contato = input("Digite o índice do contato que deseja editar: ")
     editar_contato(contatos, indice_contato)
+  elif opcao == "4":
+    ver_contatos(contatos)
+    favoritos(contatos)
+  elif opcao == "5":
+    ver_favoritos(contatos)
+  elif opcao == "6":
+    ver_contatos(contatos)
+    indice_contato = int(input("\nDigite o contato que quer excluir: "))
+    apagar_contato(contatos, indice_contato)
   elif opcao == "7":
     break
