fedaa96a37ee9136df7d3ea5e504bb235806a85d
Rodrigo Silva <jose.leandro@dotgroup.com.br>
2024-05-14 09:53:04 -0300

    feito ate a aula "pilares do poo - heranca, polimorfismo e encapsulamento - parte 2"

--- Modulo_2/pilares_poo.py
+++ Modulo_2/pilares_poo.py
@@ -8,13 +8,57 @@ class Animal:
     print(f"O animal {self.nome} andou")
     return
   
-  def emitir_son(self):
+  def emitir_som(self):
     pass
 
 class Cachorro(Animal):
-  def emitir_son(self):
+  def emitir_som(self):
     return "Au, au"
   
 class Gato(Animal):
-  def emitir_son(self):
+  def emitir_som(self):
     return "Miau!"
+  
+
+dog = Cachorro(nome="Rex")
+cat = Gato(nome="Felix")
+
+print("\nExemplo de polimorfismo")
+animais = [dog, cat]
+
+for animal in animais:
+  print(f"{animal.nome} faz: {animal.emitir_som()}")
+
+
+
+
+
+
+
+
+
+print("\nExemplo de encapsulamento:")
+class ContaBancaria:
+  def __init__(self, saldo) -> None:
+    self.__saldo = saldo #Atributo privado - somente os métodos dentro dessa classe têm acesso a ele
+
+  def depositar(self, valor):
+    if valor > 0:
+      self.__saldo += valor
+
+  def sacar(self, valor):
+    if valor > 0 and valor <= self.__saldo:
+      self.__saldo -= valor
+
+  def consultar_saldo(self):
+    return self.__saldo
+  
+
+conta = ContaBancaria(saldo=1000)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.depositar(valor=500)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.depositar(valor=-500)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.sacar(valor=200)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
